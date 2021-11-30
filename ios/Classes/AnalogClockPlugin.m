67506e294e231292c37015f99655862fee3c90c7
Anderson Freitas <anderson.santos@willbank.com.br>
2021-11-29 21:37:28 -0300

    migrated to nullsafety

--- ios/Classes/AnalogClockPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "AnalogClockPlugin.h"
-#import <analog_clock/analog_clock-Swift.h>
-
-@implementation AnalogClockPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftAnalogClockPlugin registerWithRegistrar:registrar];
-}
-@end
