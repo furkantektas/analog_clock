b692b81cce69e070178a287223f10cfe593c9ff0
Anderson Freitas <anderson.santos@willbank.com.br>
2021-11-29 21:37:28 -0300

    migrated to nullsafety

--- ios/Classes/AnalogClockPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface AnalogClockPlugin : NSObject<FlutterPlugin>
-@end
--- lib/analog_clock_painter.dart
+++ lib/analog_clock_painter.dart
@@ -24,7 +24,7 @@ class AnalogClockPainter extends CustomPainter {
   static const double STROKE_WIDTH = 3.0;
 
   AnalogClockPainter(
-      {@required this.datetime,
+      {required this.datetime,
       this.showDigitalClock = true,
       this.showTicks = true,
       this.showNumbers = true,
@@ -53,7 +53,7 @@ class AnalogClockPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(AnalogClockPainter oldDelegate) {
-    return oldDelegate?.datetime?.isBefore(datetime) ?? true;
+    return oldDelegate.datetime.isBefore(datetime);
   }
 
   _paintPinHole(canvas, size, scaleFactor) {
